8c94022d2c2e7da0415cd97702458c93e1076a1c
monika-droid <66963824+monika-droid@users.noreply.github.com>
2020-06-22 23:14:19 +0530

    Add files via upload

---
+++ src/com/monika/pojos/ProductSale.java
@@ -0,0 +1,17 @@
+package com.monika.pojos;
+
+public class ProductSale {
+
+	protected int productId;
+	protected String productName;
+	protected int productQuantity;
+	protected float price;
+	protected float cgst;
+	protected float sgst;
+	protected float total;
+	
+	public ProductSale() {
+
+	}
+
+}
---
+++ src/com/monika/ui/Table.java
@@ -0,0 +1,42 @@
+package com.monika.ui;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import com.monika.pojos.*;
+import com.monika.daos.*;
+
+public class Table extends JPanel implements ActionListener {
+	
+	
+	
+
+	public Table() {
+		setLayout(new BorderLayout());
+	
+		
+	}
+	
+
+	private void setLayout(BorderLayout borderLayout) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+public static void main(String args[]) {
+	JFrame f = new JFrame("Product");
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	f.setContentPane(new Table());
+	f.setMinimumSize(new Dimension(700, 700));
+	f.setLocationRelativeTo(null);
+	f.setResizable(false);
+	f.setVisible(true);
+}
+}
